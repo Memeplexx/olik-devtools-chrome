f82b61946c58a8cc7ed961e2f7d889391b6c89a5
Stephen Paul <stephensimonpaul@gmail.com>
2023-07-26 16:50:01 +0200

    implement before and after state preview

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,25 +1,51 @@
 import React, { MutableRefObject } from "react";
 import { Message } from "./constants";
-import { RecursiveRecord, Store, createStore, getStore } from "olik";
+import { RecursiveRecord, Store, createStore, getStore, libState } from "olik";
 
 export const useHooks = () => {
+	const hooks = useHooksInitializer();
+	useMessageReceiver(hooks);
+	useHistoryClipper(hooks);
+	return hooks;
+}
+
+const useHooksInitializer = () => {
 	const [state, setState] = React.useState<RecursiveRecord | null>(null);
 	const storeRef = React.useRef<Store<RecursiveRecord> | null>(null);
 	initializeStore({ state, storeRef });
 	const [query, setQuery] = React.useState('');
-	const [selected, setSelected] = React.useState<{before: unknown | null, after: unknown | null} | null>(null);
+	const [selected, setSelected] = React.useState<{ before: unknown | null, after: unknown | null } | null>(null);
 	const [items, setItems] = React.useState<{ type: string, typeFormatted: string, id: number, state: RecursiveRecord, last: boolean }[]>([]);
 	const [selectedId, setSelectedId] = React.useState<number | null>(null);
+	return {
+		items,
+		setItems,
+		state,
+		query,
+		setQuery,
+		setState,
+		selected,
+		setSelected,
+		storeRef,
+		selectedId,
+		setSelectedId,
+	};
+}
 
+const useMessageReceiver = (hooks: ReturnType<typeof useHooksInitializer>) => {
+	const setStateRef = React.useRef(hooks.setState);
+	const setItemsRef = React.useRef(hooks.setItems);
 	React.useEffect(() => {
-		const getInitialState = () => {
-			return JSON.parse(document.getElementById('olik-state')!.innerHTML) as RecursiveRecord;
-		}
+		const setState = setStateRef.current;
+		const setItems = setItemsRef.current;
+		const getInitialState = () => JSON.parse(document.getElementById('olik-state')!.innerHTML) as RecursiveRecord;
 		const processEvent = ({ action: { state, type, last } }: Message) => {
 			setState(state);
-			const typeFormatted = type
-				.replace(/\$[A-Za-z0-9]+/g, match => `<span class="action">${match}</span>`);
-			setItems(items => [...items, { type, typeFormatted, id: idCounter++, state, last }]);
+			const typeFormatted = type.replace(/\$[A-Za-z0-9]+/g, match => `<span class="action">${match}</span>`);
+			setItems(items => {
+				const newId = items.length ? items[items.length - 1].id + 1 : 0;
+				return [...items, { type, typeFormatted, id: newId, state, last }];
+			});
 		}
 		const messageListener = (e: MessageEvent<Message>) => {
 			if (e.origin !== window.location.origin) { return; }
@@ -33,47 +59,43 @@ export const useHooks = () => {
 			chrome.runtime.onMessage
 				.addListener((event: Message) => processEvent(event));
 			chrome.tabs
-				.query({
-					active: true
-				})
-				.then(result => chrome.scripting.executeScript({
-					target: { tabId: result[0].id! },
-					func: getInitialState,
-				}))
+				.query({ active: true })
+				.then(result => chrome.scripting.executeScript({ target: { tabId: result[0].id! }, func: getInitialState }))
 				.then(r => setState(r[0].result))
 				.catch(console.error);
 		}
-		return () => {
-			window.removeEventListener('message', messageListener);
-		}
+		return () => window.removeEventListener('message', messageListener)
 	}, []);
-
-	return {
-		items,
-		state,
-		query,
-		setQuery,
-		setState,
-		selected,
-		setSelected,
-		storeRef,
-		selectedId,
-		setSelectedId,
-	}
 }
 
 const initializeStore = (props: { state: RecursiveRecord | null, storeRef: MutableRefObject<Store<RecursiveRecord> | null> }) => {
 	if (!props.state) { return; }
-  if (!props.storeRef.current) {
-    if (!chrome.runtime) {
-      props.storeRef.current = getStore(); // get store from demo app
-    } else {
-      props.storeRef.current = createStore<RecursiveRecord>({ state: props.state });
-    }
-  }
-  if (chrome.runtime) {
-    props.storeRef.current.$set(props.state);
-  }
+	if (!props.storeRef.current) {
+		if (!chrome.runtime) {
+			props.storeRef.current = getStore(); // get store from demo app
+		} else {
+			props.storeRef.current = createStore<RecursiveRecord>({ state: props.state });
+		}
+	}
+	if (chrome.runtime) {
+		props.storeRef.current.$set(props.state);
+	}
 }
 
-let idCounter = 1;
+const useHistoryClipper = (hooks: ReturnType<typeof useHooksInitializer>) => {
+	const setSelectedIdRef = React.useRef(hooks.setSelectedId);
+	const setItemsRef = React.useRef(hooks.setItems);
+	React.useEffect(() => {
+		if (!hooks.storeRef.current) { return; }
+		const setSelectedId = setSelectedIdRef.current;
+		const setItems = setItemsRef.current;
+		const subscription = hooks.storeRef.current.$onChange(() => {
+			if (libState.disableDevtoolsDispatch) { return; }
+			if (!hooks.selectedId) { return; }
+			setSelectedId(null);
+			const index = hooks.items.findIndex(e => e.id === hooks.selectedId);
+			setItems(items => items.slice(0, index + 1));
+		});
+		return () => subscription.unsubscribe();
+	}, [hooks.items, hooks.selectedId, hooks.storeRef]);
+}
