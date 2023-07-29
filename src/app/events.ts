27232d990b521bfce14ed0b467370fa54ba012f0
Stephen Paul <stephensimonpaul@gmail.com>
2023-07-29 11:22:22 +0200

    Minor updates

--- src/app/events.ts
+++ src/app/events.ts
@@ -8,27 +8,27 @@ export const useEvents = (props: ReturnType<typeof useHooks>) => ({
   },
   onMouseEnterItem: (id: number) => () => {
     const itemIndex = props.items.findIndex(item => item.id === id);
-    const item = props.items[itemIndex];
-    props.setQuery(item.type);
+    const itemAfter = props.items[itemIndex];
+    props.setQuery(itemAfter.type);
     const itemBefore = itemIndex === 0
       ? { type: '', state: libState.initialState }
       : props.items.slice(0, itemIndex).reverse().find(item => item.last)!
     props.setSelected({
-      before: doReadState(item.type, itemBefore.state),
-      after: doReadState(item.type, item.state),
+      before: doReadState(itemAfter.type, itemBefore.state),
+      after: doReadState(itemAfter.type, itemAfter.state),
     });
-    silentlyUpdateStoreState(props, item.state);
+    silentlyUpdateAppStoreState(props, itemAfter.state);
   },
   onMouseLeaveItem: () => {
     if (props.selectedId) {
-      const item = props.items.find(item => item.id === props.selectedId)!;
-      props.setQuery(item.type);
+      const itemAfter = props.items.find(item => item.id === props.selectedId)!;
+      props.setQuery(itemAfter.type);
       props.setSelected(null);
-      silentlyUpdateStoreState(props, item.state);
+      silentlyUpdateAppStoreState(props, itemAfter.state);
     } else {
       props.setQuery('');
       props.setSelected(null);
-      silentlyUpdateStoreState(props, props.items[props.items.length - 1].state);
+      silentlyUpdateAppStoreState(props, props.items[props.items.length - 1].state);
     }
   },
   onClickItem: (id: number) => () => {
@@ -36,7 +36,7 @@ export const useEvents = (props: ReturnType<typeof useHooks>) => ({
   }
 })
 
-const silentlyUpdateStoreState = (props: ReturnType<typeof useHooks>, state: RecursiveRecord) => {
+const silentlyUpdateAppStoreState = (props: ReturnType<typeof useHooks>, state: RecursiveRecord) => {
   if (!chrome.runtime) {
     libState.disableDevtoolsDispatch = true;
     props.storeRef.current!.$set(state);
