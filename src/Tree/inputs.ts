4506839551ea719cf11e416cadba28a9a16172c9
Memeplexx <stephensimonpaul@gmail.com>
2024-05-12 17:53:50 +0200

    Refactor useDerivedState function in inputs.ts to use destructuring and arrow function syntax

--- src/Tree/inputs.ts
+++ src/Tree/inputs.ts
@@ -26,17 +26,17 @@ export const useLocalState = (
   key: '',
   value: props.item as InputValue,
   type: (() => {
-    if (is.number(props.item)) 
+    if (is.number(props.item))
       return 'number';
-    if (is.string(props.item)) 
+    if (is.string(props.item))
       return 'string';
-    if (is.boolean(props.item)) 
+    if (is.boolean(props.item))
       return 'boolean';
-    if (is.date(props.item)) 
+    if (is.date(props.item))
       return 'date';
-    if (is.null(props.item)) 
+    if (is.null(props.item))
       return 'null';
-    if (is.undefined(props.item)) 
+    if (is.undefined(props.item))
       return 'undefined';
     return 'string';
   })() as ValueType,
@@ -46,76 +46,98 @@ export const useLocalState = (
 const useDerivedState = (
   props: RenderNodeArgs,
   state: ReturnType<typeof useLocalState>,
-) => ({
-  isPrimitive: !is.array(props.item) && !is.record(props.item),
-  hasObjectKey: props.objectKey !== undefined,
-  showActionType: props.isTopLevel && !!props.actionType,
-  isUnchanged: useMemo(() => {
+) => {
+  const isPrimitive = !is.array(props.item) && !is.record(props.item);
+  const hasObjectKey = props.objectKey !== undefined;
+  const showActionType = props.isTopLevel && !!props.actionType;
+  const isUnchanged = useMemo(() => {
     return props.unchanged.includes(props.keyConcat);
-  }, [props.keyConcat, props.unchanged]),
-  isContracted: useMemo(() => {
+  }, [props.keyConcat, props.unchanged]);
+  const isContracted = useMemo(() => {
     return props.contractedKeys.includes(props.keyConcat);
-  }, [props.contractedKeys, props.keyConcat]),
-  isHidden: useMemo(() => {
+  }, [props.contractedKeys, props.keyConcat]);
+  const isHidden = useMemo(() => {
     return props.unchanged.includes(props.keyConcat) && props.hideUnchanged;
-  }, [props.hideUnchanged, props.keyConcat, props.unchanged]),
-  isEmpty: useMemo(() => {
-    if (is.array(props.item)) 
+  }, [props.hideUnchanged, props.keyConcat, props.unchanged]);
+  const isEmpty = useMemo(() => {
+    if (is.array(props.item))
       return !props.item.length;
-    if (is.record(props.item)) 
+    if (is.record(props.item))
       return !Object.keys(props.item).length;
     return false;
-  }, [props.item]),
-  nodeEl: useMemo(() => {
-    if (is.null(props.item)) 
+  }, [props.item]);
+  const nodeEl = useMemo(() => {
+    if (is.null(props.item))
       return 'null';
-    if (is.undefined(props.item)) 
+    if (is.undefined(props.item))
       return '';
-    if (is.boolean(props.item)) 
+    if (is.boolean(props.item))
       return props.item.toString();
-    if (is.number(props.item)) 
+    if (is.number(props.item))
       return props.item.toString();
-    if (is.string(props.item)) 
+    if (is.string(props.item))
       return `"${props.item}"`;
-    if (is.date(props.item)) 
+    if (is.date(props.item))
       return props.item.toISOString();
     return props.item;
-  }, [props.item]) as ReactNode,
-  isChanged: useMemo(() => {
+  }, [props.item]) as ReactNode;
+  const isChanged = useMemo(() => {
     return props.changed.includes(props.keyConcat);
-  }, [props.changed, props.keyConcat]),
-  styles: useMemo(() => ({
-    $unchanged: props.unchanged.includes(props.keyConcat),
-    $displayInline: props.displayInline,
-    $showTextArea: state.isShowingTextArea,
-    $isArrayOrObject: is.array(props.item) || is.record(props.item),
-    $color: (() => {
-      if (props.unchanged.includes(props.keyConcat)) 
-        return 'gray';
-      if (is.array(props.item)) 
-        return 'red';
-      if (is.record(props.item)) 
-        return 'violet';
-      if (is.number(props.item)) 
-        return 'darkorange';
-      if (is.string(props.item)) 
-        return 'green';
-      if (is.date(props.item)) 
-        return 'deepskyblue';
-      if (is.null(props.item)) 
-        return 'lightblue';
-      if (is.boolean(props.item)) 
-        return '#00ff3c';
-      if (is.undefined(props.item)) 
-        return 'magenta';
-    })(),
-    $nonValueColor: (() => {
-      if (props.unchanged.includes(props.keyConcat)) 
-        return 'gray';
-      return '#e4e4e4';
-    })(),
-  }), [props.displayInline, props.item, props.keyConcat, props.unchanged, state.isShowingTextArea])
-});
+  }, [props.changed, props.keyConcat]);
+  const $displayInline = props.displayInline;
+  const $showTextArea = state.isShowingTextArea;
+  const $unchanged = useMemo(() => {
+    return props.unchanged.includes(props.keyConcat);
+  }, [props.keyConcat, props.unchanged])
+  const $isArrayOrObject = useMemo(() => {
+    return is.array(props.item) || is.record(props.item);
+  }, [props.item]);
+  const $color = useMemo(() => {
+    if (props.unchanged.includes(props.keyConcat))
+      return 'gray';
+    if (is.array(props.item))
+      return 'red';
+    if (is.record(props.item))
+      return 'violet';
+    if (is.number(props.item))
+      return 'darkorange';
+    if (is.string(props.item))
+      return 'green';
+    if (is.date(props.item))
+      return 'deepskyblue';
+    if (is.null(props.item))
+      return 'lightblue';
+    if (is.boolean(props.item))
+      return '#00ff3c';
+    if (is.undefined(props.item))
+      return 'magenta';
+  }, [props.item, props.keyConcat, props.unchanged]);
+  const $nonValueColor = useMemo(() => {
+    if (props.unchanged.includes(props.keyConcat))
+      return 'gray';
+    return '#e4e4e4';
+  }, [props.keyConcat, props.unchanged]);
+  const styles = useMemo(() => ({
+    $unchanged,
+    $displayInline,
+    $showTextArea,
+    $isArrayOrObject,
+    $color,
+    $nonValueColor,
+  }), [$unchanged, $displayInline, $showTextArea, $isArrayOrObject, $color, $nonValueColor]);
+  return {
+    isPrimitive,
+    hasObjectKey,
+    showActionType,
+    isUnchanged,
+    isContracted,
+    isHidden,
+    isEmpty,
+    nodeEl,
+    isChanged,
+    styles,
+  }
+};
 
 const useValueUpdater = (
   props: RenderNodeArgs,
